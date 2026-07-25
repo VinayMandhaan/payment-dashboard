d865f3b8edc572e2e6830ba7924cb0e433786808
VinaySAC <v.kumar@sellanycar.com>
2026-07-25 17:24:15 +0400

    Cors Added for Client Call

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import paymentRouter from './routes/payments'
 
 const app = express()
 const PORT = process.env.port || 4000
 
+app.use(cors())
 app.use('/api', paymentRouter)
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
